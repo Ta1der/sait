58830883aadb50c922a4eeafc9a006fbc6196eb8
Ta1der <demenko_slava@mail.ru>
2024-12-02 14:29:36 +0700

    абоба

--- womendevil/menu/views.py
+++ womendevil/menu/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Menu
 
+
 def index(request):
     men = Menu.objects.all()
     context = {
         "menus": men
     }
     return render(request, "menu/index.html", context)
+
+
+def menus(request):
+    return render(request, "menu/Menu.html", {})
