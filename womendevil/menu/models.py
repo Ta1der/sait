03c0257e7dc3fdd9d35f4053653da9f5e94f4f3b
Taider <demenko_slava@mail.ru>
2024-12-16 15:28:31 +0700

    алина заруинила

--- womendevil/menu/models.py
+++ womendevil/menu/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=64)
     sostav = models.CharField(max_length=256)
-    cena = models.CharField(max_length=256)
+    cena = models.CharField(max_length=256, default=0)
     image = models.ImageField(null=False)
 
     def __str__(self):
