cf0457a55e9275adb2bfe22fb0a4cca8ae2d4e12
Ta1der <demenko_slava@mail.ru>
2024-12-16 12:32:19 +0700

    АЛИНА ДУРА

--- womendevil/menu/models.py
+++ womendevil/menu/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Menu(models.Model):
     name = models.CharField(max_length=64)
     sostav = models.CharField(max_length=256)
+    cena = models.CharField(max_length=256)
     image = models.ImageField(null=False)
 
     def __str__(self):
